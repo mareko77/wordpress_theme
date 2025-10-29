3560e6a7ffe28d6e6a30ddc488f5a510caa7839f
Marek Ograbek <36967486+mareko77@users.noreply.github.com>
2025-10-29 15:40:06 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <?php wp_head(); ?>
+    <title>Document</title>
+</head>
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+
+    <p>Hello</p>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
